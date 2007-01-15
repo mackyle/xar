d26bef5b16e739fa78990b452d8308441e7f1068
Will Barton <wb@willbarton.com>
2007-01-15 04:59:23 +0000

    Updated addresses (replacing opendarwin, etc)

--- python/setup.py
+++ python/setup.py
@@ -1,3 +1,34 @@
+#
+# py-xar
+# Python Bindings for XAR, the eXtensible ARchiver
+#
+# Copyright (c) 2005, 2006 Will Barton <wb@willbarton.com>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+#   1. Redistributions of source code must retain the above copyright 
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright 
+#      notice, this list of conditions and the following disclaimer in the 
+#      documentation and/or other materials provided with the distribution.
+#   3. The name of the author may not be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+# THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Pyrex.Distutils import build_ext
@@ -8,12 +39,12 @@ xarfile = Extension('xarfile',
 
 setup(
         name = 'pyxar', 
-        version = '0.3.0',
+        version = '0.4',
         description = 'Python bindings for XAR, the eXtensible ARchiver', 
         author = 'Will Barton',
-        author_email = 'wbb4@opendarwin.org',
-        url = 'http://www.opendarwin.org/projects/xar/python',
-        license = 'BSD-style License',
+        author_email = 'wb@willbarton.com',
+        url = 'http://code.google.com/p/xar',
+        license = 'BSD',
         ext_modules = [xarfile],
         cmdclass = {'build_ext':build_ext}
 )
