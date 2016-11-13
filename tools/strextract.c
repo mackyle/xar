43b92a6690a9040500b47cf973bef3b4f83361b6
Jakub Kaszycki <kuba@kaszycki.net.pl>
2016-11-13 15:12:19 +0100

    Re-indented source files

--- tools/strextract.c
+++ tools/strextract.c
@@ -4,84 +4,98 @@
 #include <xar/xar.h>
 #include <sys/fcntl.h>
 
-static int32_t err_callback(int32_t sev, int32_t err, xar_errctx_t ctx, void *usrctx) {
-	fprintf(stderr, "err callback\n");
+static int32_t
+err_callback (int32_t sev, int32_t err, xar_errctx_t ctx, void *usrctx)
+{
+  fprintf (stderr, "err callback\n");
 }
 
-int main(int argc, char *argv[]) {
-	xar_t x;
-	xar_iter_t i;
-	xar_file_t f;
-	xar_stream s;
-	char buffer[8192];
+int
+main (int argc, char *argv[])
+{
+  xar_t x;
+  xar_iter_t i;
+  xar_file_t f;
+  xar_stream s;
+  char buffer[8192];
 
-	x = xar_open(argv[1], READ);
-	if( !x ) {
-		fprintf(stderr, "Error opening archive\n");
-		exit(1);
-	}
+  x = xar_open (argv[1], READ);
+  if (!x)
+    {
+      fprintf (stderr, "Error opening archive\n");
+      exit (1);
+    }
 
-	xar_register_errhandler(x, err_callback, NULL);
+  xar_register_errhandler (x, err_callback, NULL);
 
-	i = xar_iter_new();
-	if( !i ) {
-		fprintf(stderr, "Error creating xar iterator\n");
-		exit(1);
-	}
+  i = xar_iter_new ();
+  if (!i)
+    {
+      fprintf (stderr, "Error creating xar iterator\n");
+      exit (1);
+    }
 
-	for(f = xar_file_first(x, i); f; f = xar_file_next(i)) {
-		char *path;
-		const char *type;
-		int32_t ret;
-		int fd;
+  for (f = xar_file_first (x, i); f; f = xar_file_next (i))
+    {
+      char *path;
+      const char *type;
+      int32_t ret;
+      int fd;
 
-		path = xar_get_path(f);
+      path = xar_get_path (f);
 
-		xar_prop_get(f, "type", &type);
-		if( !type ) {
-			fprintf(stderr, "File has no type %s\n", path);
-			free(path);
-			continue;
-		}
-		if( strcmp(type, "file") != 0 ) {
-			fprintf(stderr, "Skipping %s\n", path);
-			free(path);
-			continue;
-		}
-	
-		fprintf(stderr, "Extracting %s\n", path);
-		if( xar_extract_tostream_init(x, f, &s) != XAR_STREAM_OK ) {
-			fprintf(stderr, "Error initializing stream %s\n", path);
-			free(path);
-			continue;
-		}
-		
-		fd = open(path, O_CREAT|O_WRONLY, S_IRUSR|S_IWUSR);
-		if( !fd ) {
-			fprintf(stderr, "Error opening output file %s\n", path);
-			free(path);
-			continue;
-		}
-		s.avail_out = sizeof(buffer);
-		s.next_out = buffer;
+      xar_prop_get (f, "type", &type);
+      if (!type)
+        {
+          fprintf (stderr, "File has no type %s\n", path);
+          free (path);
+          continue;
+        }
+      if (strcmp (type, "file") != 0)
+        {
+          fprintf (stderr, "Skipping %s\n", path);
+          free (path);
+          continue;
+        }
 
-		while( (ret = xar_extract_tostream(&s)) != XAR_STREAM_END ) {
-			if( ret == XAR_STREAM_ERR ) {
-				fprintf(stderr, "Error extracting stream %s\n", path);
-				exit(2);
-			}
+      fprintf (stderr, "Extracting %s\n", path);
+      if (xar_extract_tostream_init (x, f, &s) != XAR_STREAM_OK)
+        {
+          fprintf (stderr, "Error initializing stream %s\n", path);
+          free (path);
+          continue;
+        }
 
-			write(fd, buffer, sizeof(buffer)-s.avail_out);
+      fd = open (path, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+      if (!fd)
+        {
+          fprintf (stderr, "Error opening output file %s\n", path);
+          free (path);
+          continue;
+        }
+      s.avail_out = sizeof (buffer);
+      s.next_out = buffer;
 
-			s.avail_out = sizeof(buffer);
-			s.next_out = buffer;
-		}
+      while ((ret = xar_extract_tostream (&s)) != XAR_STREAM_END)
+        {
+          if (ret == XAR_STREAM_ERR)
+            {
+              fprintf (stderr, "Error extracting stream %s\n", path);
+              exit (2);
+            }
 
-		if( xar_extract_tostream_end(&s) != XAR_STREAM_OK ) {
-			fprintf(stderr, "Error ending stream %s\n", path);
-		}
+          write (fd, buffer, sizeof (buffer) - s.avail_out);
 
-		close(fd);
-		free(path);
-	}
+          s.avail_out = sizeof (buffer);
+          s.next_out = buffer;
+        }
+
+      if (xar_extract_tostream_end (&s) != XAR_STREAM_OK)
+        {
+          fprintf (stderr, "Error ending stream %s\n", path);
+        }
+
+      close (fd);
+      free (path);
+    }
 }
